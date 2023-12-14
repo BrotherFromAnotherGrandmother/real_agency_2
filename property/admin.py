f4526190ca5859c66aba70e41f85093417ba91a8
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-12-14 15:12:36 +0300

    Исправил ошибку

--- property/admin.py
+++ property/admin.py
@@ -14,7 +14,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
-
+    inlines = [OwnerInline, ]
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', 'flat',)
@@ -22,7 +22,6 @@ class ComplaintAdmin(admin.ModelAdmin):
 
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats', )
-    inlines = [OwnerInline, ]
 
 
 admin.site.register(Flat, FlatAdmin)
